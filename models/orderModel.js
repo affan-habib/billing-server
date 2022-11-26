373adfd71e62cbc98382cc34d169f6e81bd462de
Affan-Habib <affan.habib44@gmail.com>
2022-11-26 20:51:33 +0600

    order route created

---
+++ models/orderModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema(
+  {
+   
+    patientId: {
+      type: String,
+    },
+    total: {
+      type: Number,
+    },
+    discount: {
+      type: Number,
+    },
+    due: {
+      type: Number,
+    },
+    orderDetailList: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
