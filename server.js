173a2dc9d3285bc2e94e68fdb18258ca2218b41a
Affan-Habib <affan.habib44@gmail.com>
2022-12-11 21:14:07 +0600

    up

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const { errorHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -14,13 +12,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1/service-master/items', require('./routes/goalRoutes'));
-app.use('/api/customers', require('./routes/customerRoutes'));
-app.use('/api/orders', require('./routes/orderRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
+app.use("/api/v1/service-master/items", require("./routes/goalRoutes"));
+app.use("/api/customers", require("./routes/customerRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
-
 
 app.use(errorHandler);
 
